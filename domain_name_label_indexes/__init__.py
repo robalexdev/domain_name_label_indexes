17865573aedf957facec575412c3113670c379e7
Robert Alexander <3751418+ralexander-phi@users.noreply.github.com>
2022-12-26 16:34:43 +0400

    black: formatting

--- domain_name_label_indexes/__init__.py
+++ domain_name_label_indexes/__init__.py
@@ -2,50 +2,54 @@ import string
 import functools
 
 __author__ = """Robert Alexander"""
-__email__ = 'raalexander.phi@gmail.com'
-__version__ = '0.1.0'
+__email__ = "raalexander.phi@gmail.com"
+__version__ = "0.1.0"
 
 
 _MAX_LABEL_LENGTH = 63
 _DOMAIN_CHARS = string.digits + string.ascii_lowercase
 _DOMAIN_BASE = len(_DOMAIN_CHARS)
-_DOMAIN_CHARS_WITH_HYPHEN = '-' + _DOMAIN_CHARS
+_DOMAIN_CHARS_WITH_HYPHEN = "-" + _DOMAIN_CHARS
+
 
 def count_domains_with_length(length: int) -> int:
     if length <= 0 or length > 63:
         raise Exception("Invalid unput")
     if length < 3:
         return _DOMAIN_BASE**length
-    return _DOMAIN_BASE**2 * _count_domains_with_length_r(length-2)
+    return _DOMAIN_BASE**2 * _count_domains_with_length_r(length - 2)
+
 
 # recursive helper
 @functools.cache
 def _count_domains_with_length_r(length: int) -> int:
     if length == 1:
-        return (
-            _DOMAIN_BASE + # any base character
-            1              # or a hyphen
-        )
+        return _DOMAIN_BASE + 1  # any base character  # or a hyphen
     if length == 2:
         return (
-            _DOMAIN_BASE * _DOMAIN_BASE + # base base
-            _DOMAIN_BASE * 1            + # base hyphen
-            1 * _DOMAIN_BASE              # hyphen base
+            _DOMAIN_BASE * _DOMAIN_BASE
+            + _DOMAIN_BASE * 1  # base base
+            + 1 * _DOMAIN_BASE  # base hyphen  # hyphen base
         )
 
-    return (
-        _DOMAIN_BASE * _count_domains_with_length_r(length-1) +   # base (any n-1 sequence)
-        1 * _DOMAIN_BASE * _count_domains_with_length_r(length-2) # hyphen base (any n-2 sequence)
-    )
+    return _DOMAIN_BASE * _count_domains_with_length_r(
+        length - 1
+    ) + 1 * _DOMAIN_BASE * _count_domains_with_length_r(  # base (any n-1 sequence)
+        length - 2
+    )  # hyphen base (any n-2 sequence)
+
 
 @functools.cache
 def first_index_with_domain_length(length: int) -> int:
     if length > _MAX_LABEL_LENGTH or length < 1:
-        raise Exception(f"Supported domain name labels are between 1 and 63 characters long, requested {length}")
+        raise Exception(
+            f"Supported domain name labels are between 1 and 63 characters long, requested {length}"
+        )
     sum = 0
     for i in range(1, length):
         sum += count_domains_with_length(i)
     return sum
+
 
 def compute_domain_length(index: int) -> int:
     for n in range(1, _MAX_LABEL_LENGTH + 1):
@@ -55,43 +59,62 @@ def compute_domain_length(index: int) -> int:
         else:
             return n
     else:
-        raise Exception("DNS wire format does not support domains longer than 63 characters")
+        raise Exception(
+            "DNS wire format does not support domains longer than 63 characters"
+        )
+
 
 def generate_domain_name_label(index: int):
     len_remaining = compute_domain_length(index)
     return _generate_domain_name_label_r(index, len_remaining, True)
 
-def _generate_domain_name_label_r(index: int, len_remaining: int, is_first: bool) -> str:
+
+def _generate_domain_name_label_r(
+    index: int, len_remaining: int, is_first: bool
+) -> str:
     if len_remaining == 1:
         return _DOMAIN_CHARS[index]
     elif len_remaining == 2:
         last_char = _DOMAIN_CHARS[index % _DOMAIN_BASE]
         index = index // _DOMAIN_BASE
         if is_first:
-            index -= 1  # trim an extra, we'll avoid padding leading zeros on earlier numbers
+            index -= (
+                1  # trim an extra, we'll avoid padding leading zeros on earlier numbers
+            )
         first_char = _DOMAIN_CHARS[index]
         return first_char + last_char
     else:
         # Process from the next most significant character
-        first_char, index = _trim_most_significant_as_alphanumeric(index, len_remaining, is_first)
+        first_char, index = _trim_most_significant_as_alphanumeric(
+            index, len_remaining, is_first
+        )
 
         # from here, we can either have a hypen followed by any valid domain of len-2
         # or any valid domain of len-1
         # let's check if this should have a hyphen first, as it's the smallest character
 
-        domain_suffixes_with_hyphen = count_domains_with_length(len_remaining-2)
+        domain_suffixes_with_hyphen = count_domains_with_length(len_remaining - 2)
         has_hyphen_next = index < domain_suffixes_with_hyphen
 
         if has_hyphen_next:
             # we have a zero value in the highest character, this maps to hyphen
-            return first_char + "-" + _generate_domain_name_label_r(index, len_remaining - 2, False)
+            return (
+                first_char
+                + "-"
+                + _generate_domain_name_label_r(index, len_remaining - 2, False)
+            )
         else:
             # the next char needs to be an alphanumeric, so the remaining is a valid domain
             # divide off extra as we already covered some with the leading hyphen
             index -= domain_suffixes_with_hyphen
-            return first_char + _generate_domain_name_label_r(index, len_remaining - 1, False)
+            return first_char + _generate_domain_name_label_r(
+                index, len_remaining - 1, False
+            )
 
-def _trim_most_significant_as_alphanumeric(index: int, len_remaining: int, is_first: bool):
+
+def _trim_most_significant_as_alphanumeric(
+    index: int, len_remaining: int, is_first: bool
+):
     if is_first:
         # we need to reduce this as the first domain of a given length
         # comes after fall the domains of shorter lengths
@@ -107,6 +130,7 @@ def _trim_most_significant_as_alphanumeric(index: int, len_remaining: int, is_fi
     remainder = index % increment_divisor
     return first_char, remainder
 
+
 def index_of_domain_name_label(label: str) -> int:
     if "--" in label:
         raise Exception("A domain name label cannot contain two hyphens in a row")
@@ -117,7 +141,7 @@ def index_of_domain_name_label(label: str) -> int:
 
     # A binary search is fast enough for now
     lower_bound = first_index_with_domain_length(len(label))
-    upper_bound = first_index_with_domain_length(len(label)+1)
+    upper_bound = first_index_with_domain_length(len(label) + 1)
 
     while lower_bound + 1 < upper_bound:
         guess = (lower_bound + upper_bound) // 2
@@ -140,4 +164,3 @@ def index_of_domain_name_label(label: str) -> int:
     if guessed_domain == label:
         return upper_bound
     assert False
-
--- tests/test_domain_name_label_indexes.py
+++ tests/test_domain_name_label_indexes.py
@@ -4,92 +4,97 @@
 
 import pytest
 
-from domain_name_label_indexes import count_domains_with_length, generate_domain_name_label, _DOMAIN_CHARS_WITH_HYPHEN, _DOMAIN_CHARS, compute_domain_length, first_index_with_domain_length, index_of_domain_name_label
+from domain_name_label_indexes import (
+    count_domains_with_length,
+    generate_domain_name_label,
+    _DOMAIN_CHARS_WITH_HYPHEN,
+    _DOMAIN_CHARS,
+    compute_domain_length,
+    first_index_with_domain_length,
+    index_of_domain_name_label,
+)
 
 
 def test_success():
     assert True
 
+
 def test_single_char_domain():
     assertEqual(36, count_domains_with_length(1))
 
+
 def test_double_char_domain():
-    assertEqual(36*36, count_domains_with_length(2))
+    assertEqual(36 * 36, count_domains_with_length(2))
+
 
 def test_triple_char_domain():
-    assertEqual(36*37*36, count_domains_with_length(3))
+    assertEqual(36 * 37 * 36, count_domains_with_length(3))
+
 
 def test_quadruple_char_domain():
     assertEqual(
-        36 * # first character
-        (
-            36 * 36 + # BB
-            36 *  1 + # B-
-                1 * 36   # -B
-        ) *
-        36, # last character
-        count_domains_with_length(4)
+        36
+        * (36 * 36 + 36 * 1 + 1 * 36)  # first character  # BB  # B-  # -B
+        * 36,  # last character
+        count_domains_with_length(4),
     )
+
 
 def test_quintuple_char_domain():
     assertEqual(
-        36 * # first character
-        (
-            36 * 36 * 36 + # BBB
-            36 * 1 * 36  + # B-B
-            1 * 36 * 36  + # -BB
-            36 * 36 * 1  + # BB-
-            1 * 36 * 1     # -B-
-        ) *
-        36,   # last character
-        count_domains_with_length(5)
+        36
+        * (  # first character
+            36 * 36 * 36
+            + 36 * 1 * 36  # BBB
+            + 1 * 36 * 36  # B-B
+            + 36 * 36 * 1  # -BB
+            + 1 * 36 * 1  # BB-  # -B-
+        )
+        * 36,  # last character
+        count_domains_with_length(5),
     )
+
 
 def test_sextuple_char_domain():
     assertEqual(
-        36 * # first character
-        (
-            36 * 36 * 36 * 36 + # BBBB
-
-            1  * 36 * 36 * 36 + # -BBB
-            36 *  1 * 36 * 36 + # B-BB
-            36 * 36 *  1 * 36 + # BB-B
-            36 * 36 * 36 *  1 + # BBB-
-
-                1 * 36 *  1 * 36 + # -B-B
-                1 * 36 * 36 *  1 + # -BB-
-            36 *  1 * 36 *  1   # B-B-
-        ) *
-        36,   # last character
-        count_domains_with_length(6)
+        36
+        * (  # first character
+            36 * 36 * 36 * 36
+            + 1 * 36 * 36 * 36  # BBBB
+            + 36 * 1 * 36 * 36  # -BBB
+            + 36 * 36 * 1 * 36  # B-BB
+            + 36 * 36 * 36 * 1  # BB-B
+            + 1 * 36 * 1 * 36  # BBB-
+            + 1 * 36 * 36 * 1  # -B-B
+            + 36 * 1 * 36 * 1  # -BB-  # B-B-
+        )
+        * 36,  # last character
+        count_domains_with_length(6),
     )
+
 
 def test_septtuple_char_domain():
     assertEqual(
-        36 * # first character
-        (
-            36 * 36 * 36 * 36 * 36 + # BBBBB
-
-                1 * 36 * 36 * 36 * 36 + # -BBBB
-            36 *  1 * 36 * 36 * 36 + # B-BBB
-            36 * 36 *  1 * 36 * 36 + # BB-BB
-            36 * 36 * 36 *  1 * 36 + # BBB-B
-            36 * 36 * 36 * 36 *  1 + # BBBB-
-
-                1 * 36 *  1 * 36 * 36 + # -B-BB
-                1 * 36 * 36 *  1 * 36 + # -BB-B
-                1 * 36 * 36 * 36 *  1 + # -BBB-
-
-                36 * 1 * 36 *  1 * 36 + # B-B-B
-                36 * 1 * 36 * 36 *  1 + # B-BB-
-
-                36 * 36 * 1 * 36 *  1 + # BB-B-
-
-                1 * 36 * 1 * 36 *  1   # -B-B-
-        ) *
-        36,   # last character
-        count_domains_with_length(7)
+        36
+        * (  # first character
+            36 * 36 * 36 * 36 * 36
+            + 1 * 36 * 36 * 36 * 36  # BBBBB
+            + 36 * 1 * 36 * 36 * 36  # -BBBB
+            + 36 * 36 * 1 * 36 * 36  # B-BBB
+            + 36 * 36 * 36 * 1 * 36  # BB-BB
+            + 36 * 36 * 36 * 36 * 1  # BBB-B
+            + 1 * 36 * 1 * 36 * 36  # BBBB-
+            + 1 * 36 * 36 * 1 * 36  # -B-BB
+            + 1 * 36 * 36 * 36 * 1  # -BB-B
+            + 36 * 1 * 36 * 1 * 36  # -BBB-
+            + 36 * 1 * 36 * 36 * 1  # B-B-B
+            + 36 * 36 * 1 * 36 * 1  # B-BB-
+            + 1 * 36 * 1 * 36 * 1  # BB-B-  # -B-B-
+        )
+        * 36,  # last character
+        count_domains_with_length(7),
     )
+
 
 def test_lengths_specific_values():
     assertEqual(compute_domain_length(0), 1)
@@ -100,11 +105,12 @@ def test_lengths_specific_values():
     assertEqual(compute_domain_length(36), 2)
     assertEqual(compute_domain_length(37), 2)
 
-    assertEqual(compute_domain_length(36**2+35), 2)
-    assertEqual(compute_domain_length(36**2+36), 3)
+    assertEqual(compute_domain_length(36**2 + 35), 2)
+    assertEqual(compute_domain_length(36**2 + 36), 3)
 
-    assertEqual(compute_domain_length(36*36*37+36**2+35), 3)
-    assertEqual(compute_domain_length(36*36*37+36**2+36), 4)
+    assertEqual(compute_domain_length(36 * 36 * 37 + 36**2 + 35), 3)
+    assertEqual(compute_domain_length(36 * 36 * 37 + 36**2 + 36), 4)
+
 
 def test_generation():
     assertEqual(generate_domain_name_label(0), "0")
@@ -112,25 +118,35 @@ def test_generation():
     assertEqual(generate_domain_name_label(36), "00")
     assertEqual(generate_domain_name_label(36**2 + 35), "zz")
     assertEqual(generate_domain_name_label(36**2 + 36), "0-0")
-    assertEqual(generate_domain_name_label(36*37*36 + 36**2 + 35), "zzz")
-    assertEqual(generate_domain_name_label(36*37*36 + 36**2 + 36), "0-00")
-    assertEqual(generate_domain_name_label(36*37*36 + 36**2 + 36 + 36), "0-10")
-    assertEqual(generate_domain_name_label(36*37*36 + 36**2 + 36 + 2*36), "0-20")
-    assertEqual(generate_domain_name_label(36*37*36 + 36**2 + 36 + 35*36 + 35), "0-zz")
-    assertEqual(generate_domain_name_label(36*37*36 + 36**2 + 36 + 36*36), "00-0")
+    assertEqual(generate_domain_name_label(36 * 37 * 36 + 36**2 + 35), "zzz")
+    assertEqual(generate_domain_name_label(36 * 37 * 36 + 36**2 + 36), "0-00")
+    assertEqual(generate_domain_name_label(36 * 37 * 36 + 36**2 + 36 + 36), "0-10")
+    assertEqual(
+        generate_domain_name_label(36 * 37 * 36 + 36**2 + 36 + 2 * 36), "0-20"
+    )
+    assertEqual(
+        generate_domain_name_label(36 * 37 * 36 + 36**2 + 36 + 35 * 36 + 35), "0-zz"
+    )
+    assertEqual(
+        generate_domain_name_label(36 * 37 * 36 + 36**2 + 36 + 36 * 36), "00-0"
+    )
 
     assertEqual(generate_domain_name_label(1870163), "0-zzz")
     assertEqual(generate_domain_name_label(1870164), "00-00")
+
 
 def test_first_and_lasts():
     for i in range(3, 20):
         first_of_length = first_index_with_domain_length(i)
         # previous is all zzz
         print(i)
-        assert generate_domain_name_label(first_of_length-1) == "z" * (i-1)
+        assert generate_domain_name_label(first_of_length - 1) == "z" * (i - 1)
+
 
 def test_huge_indexes():
-    FIRST_50_CHAR_INDEX = 65427785365883453123247467896816512104667636251413310915715696907770774956868
+    FIRST_50_CHAR_INDEX = (
+        65427785365883453123247467896816512104667636251413310915715696907770774956868
+    )
     assert FIRST_50_CHAR_INDEX == first_index_with_domain_length(50)
     assert generate_domain_name_label(FIRST_50_CHAR_INDEX) == "0-" * 24 + "00"
 
@@ -140,17 +156,21 @@ def test_huge_indexes():
 
     LAST_63_CHAR_INDEX = 583791014263271482476326569507663027313555716330347307932966791104859150313363146960655804883807043
     # this one intentionally fails as 64 is an invalid label length
-    #assert LAST_63_CHAR_INDEX == first_index_with_domain_length(64)-1
+    # assert LAST_63_CHAR_INDEX == first_index_with_domain_length(64)-1
     assert generate_domain_name_label(LAST_63_CHAR_INDEX) == "z" * 63
+
 
 def test_first_index():
     assertEqual(first_index_with_domain_length(1), 0)
     assertEqual(first_index_with_domain_length(2), 36)
     assertEqual(first_index_with_domain_length(3), 36 + 36**2)
-    assertEqual(first_index_with_domain_length(4), 36 + 36**2 + 36*37*36)
+    assertEqual(first_index_with_domain_length(4), 36 + 36**2 + 36 * 37 * 36)
+
 
 # This test takes a long time with large numbers
 BRUTE_TEST_STOP_AT = 1_000
+
+
 def test_matches_brute():
     index = 0
     previous = ""
@@ -169,6 +189,7 @@ def test_matches_brute():
         if index > BRUTE_TEST_STOP_AT:
             break
 
+
 def gen_brute_r(length: int):
     if length == 1:
         for c in _DOMAIN_CHARS:
@@ -177,30 +198,32 @@ def gen_brute_r(length: int):
         for prefix in _DOMAIN_CHARS_WITH_HYPHEN:
             for lower in gen_brute_r(length - 1):
                 item = prefix + lower
-                if '--' in item:
+                if "--" in item:
                     continue
                 yield item
+
 
 def gen_brute():
     length = 1
     while True:
         for item in gen_brute_r(length):
-            if item[0] == '-' or item[-1] == '-':
+            if item[0] == "-" or item[-1] == "-":
                 continue
-            elif '--' in item:
+            elif "--" in item:
                 continue
             else:
                 yield item
         length += 1
 
+
 def test_reverse():
     assert 0 == index_of_domain_name_label("0")
     assert 35 == index_of_domain_name_label("z")
     assert 36 == index_of_domain_name_label("00")
-    assert 36*36+35 == index_of_domain_name_label("zz")
-    assert 36*36+36 == index_of_domain_name_label("0-0")
-    assert 36*36+37 == index_of_domain_name_label("0-1")
-    assert 36*37*36+36*36+35 == index_of_domain_name_label("zzz")
+    assert 36 * 36 + 35 == index_of_domain_name_label("zz")
+    assert 36 * 36 + 36 == index_of_domain_name_label("0-0")
+    assert 36 * 36 + 37 == index_of_domain_name_label("0-1")
+    assert 36 * 37 * 36 + 36 * 36 + 35 == index_of_domain_name_label("zzz")
 
     YAHOO_INDEX = 64297428
     assert YAHOO_INDEX == index_of_domain_name_label("yahoo")
@@ -210,15 +233,18 @@ def test_reverse():
     assert GOOGLE_INDEX == index_of_domain_name_label("google")
     assert "google" == generate_domain_name_label(GOOGLE_INDEX)
 
+
 def assertEqual(a, b):
     assert a == b
+
 
 def assertLess(a, b):
     assert a < b
 
+
 def test_docs_examples():
-    assert generate_domain_name_label(0)  == "0"
-    assert generate_domain_name_label(1)  == "1"
+    assert generate_domain_name_label(0) == "0"
+    assert generate_domain_name_label(1) == "1"
     assert generate_domain_name_label(10) == "a"
     assert generate_domain_name_label(35) == "z"
     assert generate_domain_name_label(36) == "00"
@@ -233,6 +259,5 @@ def test_docs_examples():
     assert first_index_with_domain_length(1) == 0
     assert first_index_with_domain_length(2) == 36
 
-    assert count_domains_with_length(1) ==  36
+    assert count_domains_with_length(1) == 36
     assert count_domains_with_length(2) == 1296
-
